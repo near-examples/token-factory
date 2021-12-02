3386520ed91616396cc0ca794884b8df5be10ba8
Mike Purvis <mikedotexe@gmail.com>
2021-12-02 07:55:13 -0800

    Revert "Updates"
    This reverts commit be305fb0fc0e4f4ae3dc1fa0104edc41a5c58198.

---
+++ precache-manifest.0222b073ee6add1699858dbb12e16baa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11553ab681287356a48186ec58eb6509",
+    "url": "/token-factory/index.html"
+  },
+  {
+    "revision": "27590635b71d54815819",
+    "url": "/token-factory/static/css/main.e8348d7d.chunk.css"
+  },
+  {
+    "revision": "1a8ab4b23120374c82d7",
+    "url": "/token-factory/static/js/2.9a43c681.chunk.js"
+  },
+  {
+    "revision": "3e0b77d05c9e3512726e4a37a5559b0a",
+    "url": "/token-factory/static/js/2.9a43c681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27590635b71d54815819",
+    "url": "/token-factory/static/js/main.86da755d.chunk.js"
+  },
+  {
+    "revision": "744215c6a1fc50a84d7e",
+    "url": "/token-factory/static/js/runtime-main.e58d8f2b.js"
+  }
+]);
